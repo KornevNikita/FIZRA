ebafd730bc5aac2feb75a6e8d72de3efa2165fc8
KornevNikita <copperskylark@gmail.com>
2018-12-05 22:03:58 +0300

    add lightning

--- scripts/versus.js
+++ scripts/versus.js
@@ -22,8 +22,11 @@ function getResponse() {
  function runUpdate() {
 	if (timeOld == timeNew) return;
 
-	  if ($('#mm').get("innerHTML") != mm) {
-		$('#mm').set("innerHTML", mm);
+	if ($('#mm').get("innerHTML") != mm) {
+	    $('.quarter').animate({ $opacity: '0'}, 1000).then(function () {
+	        $('#mm').set("innerHTML", mm);
+	        $('.quarter').animate({ $opacity: '1' }, 1000).then(function () { updating = false; });
+	    });
 	}
 
 	if ($('#p1').get("innerHTML") != p1 || $('#p2').get("innerHTML") != p2) {
@@ -32,11 +35,25 @@ function getResponse() {
 			$('#p2').set("innerHTML", p2);
 	}
 
-	if ($('#s1').get("innerHTML") != s1 || $('#s2').get("innerHTML") != s2) {
+	if ($('#s1').get("innerHTML") != s1) {
 
-			$('#s1').set("innerHTML", s1);
-			$('#s2').set("innerHTML", s2);
+			$('.scores1').animate({ $animation: 'running shadow1 2s infinite alternate'}, 4000).then(function () {
+			 $('#s1').set("innerHTML", s1);
+			// $('.scores1').animate({ $animation: 'running shadow1 2s infinite alternate-reverse' }, 1900).then(function () {
+			$('.scores1').animate({ $animation: 'paused shadow1' }, 0).then(function () { updating = false; }) });
 	}
+
+
+
+
+	if ($('#s2').get("innerHTML") != s2) {
+
+			$('.scores2').animate({ $animation: 'running shadow1 2s infinite alternate'}, 4000).then(function () {
+			 $('#s2').set("innerHTML", s2);
+			// $('.scores1').animate({ $animation: 'running shadow1 2s infinite alternate-reverse' }, 1900).then(function () {
+			$('.scores2').animate({ $animation: 'paused shadow1' }, 0).then(function () { updating = false; }) });
+	}
+
 
 		if ($('#gg').get("innerHTML") != gg) {
 				$('#gg').set("innerHTML", gg);
