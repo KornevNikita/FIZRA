17b14ef8dcd32190a1de1823b6196f351e9eca98
381706-1Golubev <Vladgolube1999@rambler.ru>
2018-12-05 14:10:44 +0300

    JJopa

--- scripts/versus.js
+++ scripts/versus.js
@@ -3,6 +3,7 @@ var upSpeed = 5000;
 var inSpeed = 1000;
 var outSpeed = 500;
 
+
 var p1, p2, s1, s2, mm, gg;
 
 $(function() {
@@ -31,21 +32,27 @@ function getResponse() {
 	} */
 	
 	if ($('#p1').get("innerHTML") != p1 || $('#p2').get("innerHTML") != p2) {
-		/* updating = true; */
-		/* $('.players').then(function() {  */
-			$('#p1').set("innerHTML", p1);
-			$('#p2').set("innerHTML", p2);
+	    /* $('.players').then(function() {  */
+		// $('.player').animate({ $top: '-40px' }, 500).then(function () {                    - анимация (плохая)
+		     $('#p1').set("innerHTML", p1);
+		     $('#p2').set("innerHTML", p2);
+	    // $('.player').animate({ $top: '0' }, 1500).then(function () { updating = false; }); - анимация (плохая)
+	    // });                                                                                - анимация (плохая)
 			/* $('.players').then(function() { updating = false; }); 
 		}); */
 	}
 	
-	if ($('#s1').get("innerHTML") != s1 || $('#s2').get("innerHTML") != s2) {
-		/* updating = true;
-		$('.scores').then(function() {  */
-			$('#s1').set("innerHTML", s1);
+	if ($('#s1').get("innerHTML") != s1) {
+	    $('.scores1').animate({ $color: '#000080' }, 500).then(function () {
+	        $('#s1').set("innerHTML", s1);
+	        $('.scores1').animate({$color: 'white'}, 1000).then(function () { updating = false; });
+	    });
+	}
+	if ($('#s2').get("innerHTML") != s2) {
+	    $('.scores2').animate({ $top: '-40px' }, 500).then(function () {
 			$('#s2').set("innerHTML", s2);
-			/* $('.scores').then(function() { updating = false; }); 
-		});  */
+			$('.scores2').animate({ $top: '0' }, 1500).then(function () { updating = false; });
+	    });
 	}
 	
 	/* if ($('#gg').get("innerHTML") != gg) {
